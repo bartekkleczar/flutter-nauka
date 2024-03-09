59235fc3755c247d77fe5ba7df046822a199cb71
Bartosz Klęczar <128930025+bartekkleczar@users.noreply.github.com>
2024-03-09 22:43:31 +0100

    init

---
+++ navigation/lib/main.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Navigation',
+      theme: ThemeData(
+        primarySwatch: Colors.blue
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Navigation'),
+        ),
+        body: Center(
+          child: ElevatedButton(
+            onPressed: (){},
+            child: const Text('Press'),
+          ),
+        ),
+      ),
+    );
+  }
+}
+
