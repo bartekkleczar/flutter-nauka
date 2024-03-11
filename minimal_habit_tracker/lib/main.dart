a5b374f370f91d838546e4a49d9b4abfaa4c0feb
Bartosz Klęczar <128930025+bartekkleczar@users.noreply.github.com>
2024-03-11 17:25:47 +0100

    themes

--- minimal_habit_tracker/lib/main.dart
+++ minimal_habit_tracker/lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:minimal_habit_tracker/theme/theme_provider.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(ChangeNotifierProvider(
+    create: (context) => ThemeProvider(),
+    child: const MyApp(),
+  ));
 }
 
-class MyApp extends StatefulWidget{
+class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
   @override
@@ -12,11 +17,10 @@ class MyApp extends StatefulWidget{
 }
 
 class _MyApp extends State<MyApp> {
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        debugShowCheckedModeBanner: false,
+      debugShowCheckedModeBanner: false,
       home: Scaffold(),
     );
   }
